bba86b63f7ac08f56b4b32261665bee3305cfd81
Anabella Spinelli <anabellaspinelli@gmail.com>
2018-04-25 08:49:39 +0200

    Replace github with typeform and strip off unnecessary code.

--- test/package.test.js
+++ test/package.test.js
@@ -1,16 +1,14 @@
 /* global describe, it, expect */
 
-var strategy = require('..');
+var strategy = require('..')
 
-describe('passport-github', function() {
-    
-  it('should export Strategy constructor', function() {
-    expect(strategy.Strategy).to.be.a('function');
-  });
-    
-  it('should export Strategy constructor as module', function() {
-    expect(strategy).to.be.a('function');
-    expect(strategy).to.equal(strategy.Strategy);
-  });
-  
-});
+describe('passport-github', function () {
+  it('should export Strategy constructor', function () {
+    expect(strategy.Strategy).to.be.a('function')
+  })
+
+  it('should export Strategy constructor as module', function () {
+    expect(strategy).to.be.a('function')
+    expect(strategy).to.equal(strategy.Strategy)
+  })
+})
